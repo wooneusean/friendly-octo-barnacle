989f5239dd83650deba62856f8246731e9ce9d1e
woon <euseanwoon2016@gmail.com>
2021-01-23 01:12:31 +0800

    Midway thru the color space shit

--- color_space.py
+++ color_space.py
@@ -2,6 +2,26 @@ import cv2 as cv
 
 img = cv.imread('photos/cat.jpg')
 
-cv.imshow('Cat', img)
+# BGR to Grayscale
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+# cv.imshow('HSV', hsv)
+
+# BGR to L*a*b
+# lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+# cv.imshow('LAB', lab)
+
+# BGR to RGB
+# rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+# cv.imshow('RGB', rgb)
+
+# HSV to BGR
+bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+cv.imshow('BGR', bgr)
+
+# https://youtu.be/oXlwWbU8l2o?t=4902
 
 cv.waitKey(0)
