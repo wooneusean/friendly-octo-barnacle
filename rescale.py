e7e02ce59a6e58c7ad04b24cdd2ad27401bd50ec
woon <euseanwoon2016@gmail.com>
2021-01-22 13:43:08 +0800

    test new branch 2

---
+++ rescale.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
