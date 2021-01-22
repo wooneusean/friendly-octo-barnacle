00d5d7edbc58bbbf9f86858646d320203fdc4df5
woon <euseanwoon2016@gmail.com>
2021-01-22 23:49:03 +0800

    finished the contour shit

--- contours.py
+++ contours.py
@@ -1,9 +1,27 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread('photos/cat.jpg')
 
-cv.imshow('Cat', img)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-# https://youtu.be/oXlwWbU8l2o?t=3490
+blank = np.zeros(img.shape, dtype='uint8')
+
+blurred = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
+
+canny = cv.Canny(blurred, 75, 100)
+
+# ret, thresh = cv.threshold(gray, 105, 255, cv.THRESH_BINARY)
+
+cv.imshow('Cat', canny)
+
+contours, hierarchies = cv.findContours(
+    canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+print(f'{len(contours)} contour(s) found!')
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 1, cv.LINE_AA)
+
+cv.imshow('Contours drawn', blank)
 
 cv.waitKey(0)
