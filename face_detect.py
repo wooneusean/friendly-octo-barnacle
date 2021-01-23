8c91be023386578e11bd065086bbe998b1540967
woon <euseanwoon2016@gmail.com>
2021-01-24 00:13:36 +0800

    bruh this some cool ass shit wtf

--- face_detect.py
+++ face_detect.py
@@ -2,10 +2,23 @@ import cv2 as cv
 import numpy as np
 from utils import rescaleFrame
 
-img = cv.imread('photos/people.jpg')
+img = cv.imread('photos/people2.jpg')
 rescaled = rescaleFrame(img, 0.5)
+sample = rescaled
 
-cv.imshow('Person', rescaled)
+gray = cv.cvtColor(sample, cv.COLOR_BGR2GRAY)
 
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(
+    gray, scaleFactor=1.1, minNeighbors=1)
+
+print(f'Number of faces = {len(faces_rect)}')
+
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(sample, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+
+
+cv.imshow('Person', sample)
 
 cv.waitKey(0)
