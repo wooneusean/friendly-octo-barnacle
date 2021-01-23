6ad6d33c02fadcd4baa1fc1627bf87f302b396ca
woon <euseanwoon2016@gmail.com>
2021-01-23 18:03:57 +0800

    masking shit

--- masking.py
+++ masking.py
@@ -1,8 +1,17 @@
 import cv2 as cv
+import numpy as np
 from utils import rescaleFrame
 
 img = cv.imread('photos/mahjong.png')
 rescaled = rescaleFrame(img)
-cv.imshow('Mahjong', rescaled)
+
+blank = np.zeros(rescaled.shape[:2], dtype='uint8')
+
+mask = cv.rectangle(
+    blank, (rescaled.shape[1]//2-600, rescaled.shape[0]//2+275), (rescaled.shape[1]//2+500, rescaled.shape[0]//2+400), 255, -1)
+
+masked = cv.bitwise_and(rescaled, rescaled, mask=mask)
+
+cv.imshow('Blank Image', masked)
 
 cv.waitKey(0)
