28436205835500bf102c3c028b95700275e0d117
woon <euseanwoon2016@gmail.com>
2021-01-23 16:10:32 +0800

    added some blurring techniques shit

---
+++ blurring_techniques.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+from utils import rescaleFrame
+
+img = cv.imread('photos/noisy.png')
+rescaled = rescaleFrame(img, 1)
+
+# Averaging
+average = cv.blur(rescaled, (7, 7))
+cv.imshow('Average', average)
+
+# Gaussian
+gauss = cv.GaussianBlur(rescaled, (7, 7), 0)
+cv.imshow('Gaussian', gauss)
+
+# Median
+median = cv.medianBlur(rescaled, 3)
+cv.imshow('Median', median)
+
+# Bilateral
+bilateral = cv.bilateralFilter(rescaled, 10, 35, 25)
+cv.imshow('Bilateral', bilateral)
+
+cv.waitKey(0)
