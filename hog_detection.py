02673dea10250f9f608ab4ce5076c926d51197ca
woon <euseanwoon2016@gmail.com>
2021-01-26 18:34:10 +0800

    hogs cant be detected reliably enuff so it is abandoned

--- hog_detection.py
+++ hog_detection.py
@@ -1,14 +1,23 @@
 import numpy as np
-import cv2
-from matplotlib import pyplot as plt
+import cv2 as cv
+import os
+from utils import rescale_frame
+
+# ABANDONED
 
 MIN_MATCH_COUNT = 10
 
-img1 = cv2.imread('box.png', 0)          # queryImage
-img2 = cv2.imread('box_in_scene.png', 0)  # trainImage
+TILE_PATH = r'photos\tiles'
+
+query = cv.imread(r'photos\mahjong.png', 0)
+train = cv.imread(os.path.join(TILE_PATH, '90k.png'), 0)
+img1 = rescale_frame(query)         # queryImage
+img2 = rescale_frame(train)         # trainImage
+
 
 # Initiate SIFT detector
-sift = cv2.SIFT()
+sift = cv.SIFT_create(700)
+orb = cv.ORB_create()
 
 # find the keypoints and descriptors with SIFT
 kp1, des1 = sift.detectAndCompute(img1, None)
@@ -18,12 +27,41 @@ FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 search_params = dict(checks=50)
 
-flann = cv2.FlannBasedMatcher(index_params, search_params)
+flann = cv.FlannBasedMatcher(index_params, search_params)
 
 matches = flann.knnMatch(des1, des2, k=2)
 
 # store all the good matches as per Lowe's ratio test.
 good = []
 for m, n in matches:
-    if m.distance < 0.7*n.distance:
+    if m.distance < 0.6*n.distance:
+        print(m)
         good.append(m)
+
+if len(good) > MIN_MATCH_COUNT:
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h, w = img1.shape
+    pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]
+                     ).reshape(-1, 1, 2)
+    dst = cv.perspectiveTransform(pts, M)
+
+    img2 = cv.polylines(img2, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+
+else:
+    print(f"Not enough matches are found - {len(good)}/{MIN_MATCH_COUNT}")
+    matchesMask = None
+
+draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                   singlePointColor=None,
+                   matchesMask=matchesMask,  # draw only inliers
+                   flags=2)
+
+img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+
+cv.imshow('image', img3)
+cv.waitKey(0)
