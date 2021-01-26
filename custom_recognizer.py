d750e086c173fce806a5692e13e87c3c790086e7
woon <euseanwoon2016@gmail.com>
2021-01-26 17:19:11 +0800

    fully added all mahjong tiles

--- custom_recognizer.py
+++ custom_recognizer.py
@@ -27,36 +27,45 @@ from utils import rescale_frame, points_are_close, named_rectangle
 
 mon = {'top': 1080 - 250, 'left': 0, 'width': 1920, 'height': 250}
 TEMPLATE_DIR = r'photos\tiles'
+
+template_imgs = {}
+
+for tile in os.listdir(TEMPLATE_DIR):
+    path_to_tile = os.path.join(TEMPLATE_DIR, tile)
+    template = cv.imread(path_to_tile)
+    rescaled_template = rescale_frame(template)
+    gray_template = cv.cvtColor(rescaled_template, cv.COLOR_BGR2GRAY)
+    template_imgs[tile[:-4]] = gray_template
+
+window_name = "Main View"
+
 with mss() as sct:
     while True:
-        img = np.array(sct.grab(mon))
-        rescaled = rescale_frame(img)
-        sample = cv.cvtColor(rescaled, cv.COLOR_BGR2GRAY)
+        screen = np.array(sct.grab(mon))
+        rescaled_screen = rescale_frame(screen)
+        gray_screen = cv.cvtColor(rescaled_screen, cv.COLOR_BGR2GRAY)
+        sample = gray_screen
 
         # Template match for multiple
-        for tile in os.listdir(TEMPLATE_DIR):
-            path_to_tile = os.path.join(TEMPLATE_DIR, tile)
-
-            template = cv.imread(path_to_tile)
-            resized_template = rescale_frame(template)
-            gray_template = cv.cvtColor(
-                resized_template, cv.COLOR_BGR2GRAY)
-            w, h = gray_template.shape[::-1]
+        for tile_name in template_imgs:
+            w, h = template_imgs[tile_name].shape[::-1]
 
             res = cv.matchTemplate(
-                sample, gray_template, cv.TM_CCOEFF_NORMED)
+                sample, template_imgs[tile_name], cv.TM_CCOEFF_NORMED)
             threshold = 0.9
             loc = np.where(res >= threshold)
             prev_pt = []
             for pt in zip(*loc[::-1]):
                 if prev_pt:
-                    if not points_are_close(pt, prev_pt, w):
-                        named_rectangle(rescaled, tile[:-4], pt, (w, h))
+                    if not points_are_close(pt, prev_pt, w-5):
+                        named_rectangle(rescaled_screen,
+                                        tile_name, pt, (w, h))
                 else:
-                    named_rectangle(rescaled, tile[:-4], pt, (w, h))
+                    named_rectangle(rescaled_screen,
+                                    tile_name, pt, (w, h))
                 prev_pt = pt
 
-        cv.imshow('Image', rescaled)
+        cv.imshow(window_name, rescaled_screen)
 
         if cv.waitKey(25) & 0xFF == ord('q'):
             cv.destroyAllWindows()
